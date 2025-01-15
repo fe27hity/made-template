fcffac8f1ec62590693578f628135ea82d3532f0
Nicolas <nicolas@linuxstar.net>
2025-01-15 23:47:14 +0100

    Added results and conclusion section

--- project/data_analysis.R
+++ project/data_analysis.R
@@ -12,14 +12,24 @@ weather_event_damages <- tbl(conn2, "weather_event_damages")
 
 grouped_table <- weather_event_damages %>%
   group_by(STATE) %>%
-  summarize(summarized_value = sum(TOTAL_CASUALITIES, na.rm = TRUE)) %>%
+  summarize(grouped_total_casualties = sum(TOTAL_CASUALITIES, na.rm = TRUE)) %>%
   collect()
 
 joined_table <- grouped_table %>%
   inner_join(political_opinion, by = c("STATE" = "GeoName"))
 joined_data <- collect(joined_table)
 
-correlation_result <- cor(joined_data$AverageOpinionTrend, joined_data$summarized_value, use = "complete.obs")
+correlation_result <- cor(joined_data$AverageOpinionTrend, joined_data$grouped_total_casualties, use = "complete.obs")
+
+plot(
+  joined_table$AverageOpinionTrend,
+  joined_table$grouped_total_casualties,
+  main = "Scatterplot of Average Opinion Trend vs. Grouped Total Casualties",
+  xlab = "Average Opinion Trend",
+  ylab = "Total Casualties",
+  pch = 19,
+  col = "blue"
+)
 
 print(paste("The correlation between var1 and var2 is:", correlation_result))
 
